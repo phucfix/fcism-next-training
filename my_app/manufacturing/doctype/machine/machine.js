b77265631e3b6699282ee88108039081720579ba
phucfix <186391026+phucfix@users.noreply.github.com>
2025-12-08 06:39:51 +0700

    refractor db structure

---
+++ my_app/manufacturing/doctype/machine/machine.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, jfids and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Machine", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ my_app/manufacturing/doctype/machine_group/machine_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, jfids and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Machine Group", {
+// 	refresh(frm) {
+
+// 	},
+// });
